262d75127961de54d207fb3c671172a59da3103d
christianSMD <christian@smdtechnologies.com>
2023-06-20 16:06:01 +0200

    basic setup

---
+++ laravel-training/app/Http/Controllers/Test.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Test as Testing;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class Test extends Controller
+{
+   /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Testing::All();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Testing::Create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Testing::find($id);
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Testing::destroy($id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function search($id)
+    {
+        return Testing::select("*")->where("product_id", "=", $id)->get();
+    }
+}
--- laravel-training/routes/api.php
+++ laravel-training/routes/api.php
@@ -1,7 +1,8 @@
 <?php
-
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Test;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('/test', [Test::class, 'index']);
+Route::get('/test/{id}', [Test::class, 'search']);
+Route::post('/test', [Test::class, 'store']);
+Route::get('delete-test/{id}', [Test::class, 'destroy']);
+
+//Clear route cache:
+Route::get('/clear-cache', function() {
+	Artisan::call('route:cache');
+    return "Routes cache has been cleared, host: ". request()->getHost() . " \n";
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
